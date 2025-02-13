8471076d554aca7221d2e3fc33187a8f6714d66f
Ravi Sankar Karuturi <ravisankarkaruturi@gmail.com>
2025-02-13 06:36:38 -0500

    QH-05:File upload and storage

---
+++ backend/scripts/setup-db.sh
@@ -0,0 +1,12 @@
+curl -X PUT "http://localhost:9200/documents" -H "Content-Type: application/json" -d '
+{
+  "mappings": {
+    "properties": {
+      "title": { "type": "text" },
+      "content": { "type": "text" },
+      "path": { "type": "text" },
+      "tags": { "type": "keyword" },
+      "created_at": { "type": "date" },
+    }
+  }
+}'
